eee466156eaac87b16e9dbf340e65113d09d8078
Dejazmach Getachew <jilogical@gmail.com>
2020-07-24 18:43:22 +0300

    Added testing file

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -2,4 +2,37 @@ module Enumerable
   def my_each
     # your code here
   end
+  def my_each_with_index
+    # your code here
+  end
+  def my_select
+    # your code here
+  end
+  def my_all
+    # your code here
+  end
+  def my_any
+    # your code here
+  end
+  def my_none
+    # your code here
+  end
+  def my_count
+    # your code here
+  end
+  def my_map
+    ary = []
+    
+    self.each do |elem|
+      ary << yield(elem)
+    end
+    
+    ary
+  end
+  def my_inject
+    # your code here
+  end
+  def multiply_els
+    # your code here
+  end
 end
---
+++ enumerable_methods_test.rb
@@ -0,0 +1,4 @@
+require './enumerable_methods.rb'
+[5, 6, 3, 2].my_map do |n|
+  puts n
+end
