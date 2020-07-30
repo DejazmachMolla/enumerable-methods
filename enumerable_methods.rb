48689d6de9d2d129ffcb9ba7d1d1ef0d30420062
Dejazmach Getachew <jilogical@gmail.com>
2020-07-30 12:34:43 +0300

    Fixed Linting Errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,15 +1,17 @@
+# rubocop: disable Metrics/ModuleLength
+# rubocop: disable Metrics/CyclomaticComplexity
+# rubocop: disable Metrics/PerceivedComplexity
 module Enumerable
   def my_each
     arr = self if self.class == Array
-    arr = self.flatten if self.class == Hash
-    arr = self.to_a if self.class == Range
+    arr = flatten if self.class == Hash
+    arr = to_a if self.class == Range
 
     count = 0
     while count < arr.length
       yield(arr[count], count)
       count += 1
     end
-    
     self
   end
 
@@ -26,12 +28,12 @@ module Enumerable
   def my_all(arg = nil)
     if block_given?
       my_each { |elem| return false if yield(elem) != true }
-      return true #If all elements satisfy the block, we just return true
+      return true
     end
 
     if arg.nil?
       my_each { |elem| return false if elem.nil? || elem == false }
-    elsif arg.kind_of? Class
+    elsif arg.is_a? Class
       my_each { |elem| return false if elem.class != arg }
     elsif arg.class == Regexp
       my_each { |elem| return false unless arg.match(elem) }
@@ -44,12 +46,12 @@ module Enumerable
   def my_any(arg = nil)
     if block_given?
       my_each { |elem| return true if yield(elem) == true }
-      return false #If no element satisfies the block, we just return false
+      return false
     end
 
     if arg.nil?
       my_each { |elem| return true if elem.nil? || elem == true }
-    elsif arg.kind_of? Class
+    elsif arg.is_a? Class
       my_each { |elem| return true if elem.class == arg }
     elsif arg.class == Regexp
       my_each { |elem| return true if arg.match(elem) }
@@ -60,56 +62,53 @@ module Enumerable
   end
 
   def my_none(arg = nil)
+    arr = self
     if block_given?
-      !self.my_any { |elem| yield(elem) }
+      !arr.my_any { |elem| yield(elem) }
     else
-      !self.my_any(arg)
+      !arr.my_any(arg)
     end
   end
 
-  
   def my_count(num = nil)
-    if !block_given? && !num
-      return self.length 
-    end
+    arr = self
+    return arr.length if !block_given? && !num
 
-    return self.my_select { |elem| elem == num }.length if num
+    return arr.my_select { |elem| elem == num }.length if num
 
-    self.my_select { |elem| yield(elem) }.length
+    arr.my_select { |elem| yield(elem) }.length
   end
 
   def my_map
     arr = []
-    
-    self.my_each do |elem|
+    self_arr = self
+    self_arr.my_each do |elem|
       arr << yield(elem)
     end
-    
     arr
   end
 
-  def my_map_proc(p = nil)
+  def my_map_proc(proc = nil)
     arr = []
-    
-    self.my_each do |elem|
+    self_arr = self
+    self_arr.my_each do |elem|
       arr << proc.call(elem)
     end
-    
     arr
   end
 
-  def my_map_proc_block(p = nil)
+  def my_map_proc_block(proc = nil)
     arr = []
-    if p
-      self.my_each do |elem|
+    self_arr = self
+    if proc
+      self_arr.my_each do |elem|
         arr << proc.call(elem)
       end
     else
-      self.my_each do |elem|
+      self_arr.my_each do |elem|
         arr << yield(elem)
       end
     end
-    
     arr
   end
 
@@ -120,6 +119,10 @@ module Enumerable
   end
 
   def multiply_els
-    self.my_inject { |res, elem| res * elem }
+    arr = self
+    arr.my_inject { |res, elem| res * elem }
   end
 end
+# rubocop: enable Metrics/ModuleLength
+# rubocop: enable Metrics/CyclomaticComplexity
+# rubocop: enable Metrics/PerceivedComplexity
--- enumerable_methods_test.rb
+++ enumerable_methods_test.rb
@@ -1,79 +1,71 @@
 require './enumerable_methods.rb'
-
+names = %w[Jhon Paul Dj Anna]
 # my_each --> the array should not be affected by the end of the operation
-["Jhon", "Paul", "Dj", "Anna"].my_each { |name|
+names.my_each do |name|
   upcased = name.upcase
-  print  upcased+ " " #To see the upcasing really works on each element
-  upcased 
-}
+  print upcased + ' ' # To see the upcasing really works on each element
+  upcased
+end
 
 # my_each_with_index --> the array should not be affected by the end of the operation
-["Jhon", "Paul", "Dj", "Anna"].my_each_with_index { |name, index|
-  indexed = name + " " + index.to_s
-  print indexed +" " #To see the index really works on each element
+names.my_each_with_index do |name, index|
+  indexed = name + ' ' + index.to_s
+  print indexed + ' ' # To see the index really works on each element
   indexed
-}
+end
 
 # my_select
-["Jhon", "Paul", "Dj", "Anna"].my_select { |name| name != "Dj" }
+names.my_select { |name| name != 'Dj' }
 
 # my_all
-["Jhon", "Paul", "Dj", "Anna"].my_all { |name| name.length >= 3 }
+names.my_all { |name| name.length >= 3 }
 [].my_all { |name| name.length >= 3 }
 # All elements of the array are strings so we expect true
-["Jhon", "Paul", "Dj", "Anna"].my_all (String)
+names.my_all String
 # 3 is not a string => all are not strings so we expect false
-["Jhon", 3, "Dj", "Anna"].my_all (String)
+['Jhon', 3, 'Dj', 'Anna'].my_all String
 # some do not contain t => we expect false
-["ant", "bear", "cat"].my_all(/t/)
+%w[ant bear cat].my_all(/t/)
 # all contain t => we expect true
-["ant", "mosquito", "cat"].my_all(/t/)
+%w[ant mosquito cat].my_all(/t/)
 # all are ant so we expect true
-["ant", "ant", "ant"].my_all("ant")
+%w[ant ant ant].my_all('ant')
 # all are not ant so we expect false
-["ant", "ant", "cat"].my_all("ant")
+%w[ant ant cat].my_all('ant')
 
 # my_any
-["Jhon", "Paul", "Dj", "Anna"].my_any { |name| name.length >= 3 }
+names.my_any { |name| name.length >= 3 }
 
 # my_none
 # None has length greater than or equal to 5 => we expect true
-["Jhon", "Paul", "Dj", "Anna"].my_none { |name| name.length >= 5 }
+names.my_none { |name| name.length >= 5 }
 # There is Dj so we expect false
-["Jhon", "Paul", "Dj", "Anna"].my_none("Dj")
+names.my_none('Dj')
 # my_count should return 3
-["Jhon", "Paul", "Dj", "Anna"].my_count { |name| name.length >= 3 }
+names.my_count { |name| name.length >= 3 }
 
 # my_count should return 4
-["Jhon", "Paul", "Dj", "Anna"].my_count
+names.my_count
 
 # my_count should return 1
-["Jhon", "Paul", "Dj", "Anna"].my_count("Jhon")
+names.my_count('Jhon')
 
 # my_map --> the array should be affected by the operation
-["Jhon", "Paul", "Dj", "Anna"].my_map { |name|
+names.my_map do |name|
   upcased = name.upcase
-  print upcased + " " # To see the upcasing really works on each element
+  print upcased + ' ' # To see the upcasing really works on each element
   upcased
-}
-
+end
 # my_map_proc
-p = Proc.new { |name|
-  upcased = name.upcase.to_s + " PROC"
-}
-["Jhon", "Paul", "Dj", "Anna"].my_map_proc(&p)
+p = proc { |name| name.upcase.to_s + ' PROC' }
+names.my_map_proc(p)
 
 # my_map_proc_block with block
-["Jhon", "Paul", "Dj", "Anna"].my_map_proc_block { |name|
-  upcased = name.upcase.to_s + " PROC_BLOCK BLOCK"
-  upcased
-}
+names.my_map_proc_block { |name| name.upcase.to_s + ' PROC_BLOCK BLOCK' }
 
 # my_map_proc_block with proc
-p = Proc.new { |name|
-  upcased = name.upcase.to_s + " PROC_BLOCK PROC "
-}
-["Jhon", "Paul", "Dj", "Anna"].my_map_proc_block(&p)
+p = proc { |name| name.upcase.to_s + ' PROC_BLOCK PROC ' }
+names.my_map_proc_block(p)
 
 # my_inject
 [1, 2, 3, 4].my_inject { |sum, n| sum + n }
